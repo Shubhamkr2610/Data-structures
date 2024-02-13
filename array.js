da70672d789d62684eb636b45c9a341cf3e43206
shubhamoverdose <shubham@overdose.digital>
2024-02-13 17:00:20 +0530

    add some method

--- array.js
+++ array.js
@@ -31,7 +31,10 @@ const numbers = [1, 2, 3, 4, 5];
 
 // const result = numbers.filter((item, index,arr)=>item>3)
 
-const result = numbers.reduce((acc, curr, index, arr) => {
-  return acc + curr;
-}, 0);
+// const result = numbers.reduce((acc, curr, index, arr) => {
+//   return acc + curr;
+// }, 0);
+
+//return the boolean value as soon as satisfies the condition
+const result = numbers.some((item)=> item>10)
 console.log("result:", result);
