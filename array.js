54adacd574cbbf06e570cf37368eeea18d6c8dd3
shubhamoverdose <shubham@overdose.digital>
2024-02-13 16:48:12 +0530

    unshift  and shift array method

--- array.js
+++ array.js
@@ -1,7 +1,15 @@
 const arr = ["apple", "banana", "orange"]
 console.log(arr)
+
+
 //add new element to the in the last
 // arr.push("cherry")
+
 //remove element from last
 // arr.pop()
-console.log(arr)
+
+//add new element from the first
+// arr.unshift("guava")
+
+//remove the element from the first
+// arr.shift()
